a77a0a91910d3eac3198f4d863421e9d7af658e9
vinayraj khunti <vinayrajkhunti@users.noreply.github.com>
2016-04-13 16:46:09 -0700

    Update search.js

--- javascript/search.js
+++ javascript/search.js
@@ -1,9 +1,9 @@
-function vinay()
+function uparrow()
 {
 	document.getElementById('img2').style.visibility= 'visible';
 	document.getElementById('img3').style.visibility= 'hidden';
 };
-function vinay1()
+function downarrow()
 {
 	document.getElementById('img2').style.visibility= 'hidden';
 	document.getElementById('img3').style.visibility= 'visible';
